0a1b46d637dacb4272a62edd436a02c65e657fd6
Mezősi József Krisztián <mezosijoci@gmail.com>
2024-09-01 22:01:11 +0200

    add plugins

--- lua/sahwel/remap.lua
+++ lua/sahwel/remap.lua
@@ -1,3 +1,6 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set({ "i", "x", "n", "s" }, "<C-s>", "<cmd>w<cr><esc>", { desc = "Save file" })
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "<leader>s", [[:%s/]])
